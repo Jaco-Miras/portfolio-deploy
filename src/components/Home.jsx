3df7c4dd0f0cc665b2554d241fafd082b68f34c8
Jaco-Miras <miras.jaco@gmail.com>
2022-06-30 23:57:21 +0800

    major-changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,20 +15,20 @@ const Home = () => {
           I'm a Frontend Developer.
         </h2>
         <p className="text-[#3A3845] py-4 max-w-[700px]">
-          I am a junior frontend developer that is willing to learn another
-          skillset of programming, development and has a enthusiast of coding
-          especially in frontend development.
+          I am a fresh graduate with a fresh mind that is willing to learn
+          another skillset of programming, development and has a enthusiast of
+          coding especially in frontend development.
         </p>
-        <div>
+        {/* <div>
           <button className="text-[#3A3845] group border-2 border-black px-6 py-3 my-2 flex items-center hover:bg-[#E83A14] hover:border-[#E83A14]">
-            <Link to="work" smooth={true} duration={500}>
+            <Link to="/work" smooth={true} duration={500}>
               View Works
             </Link>
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Slidedepot from "../assets/slidedepot.png";
+import DORSHS from "../assets/Dorshs.png";
+import GENCO from "../assets/genco.png";
+import CROWDPH from "../assets/crowdph.png";
 
 const Work = () => {
   return (
@@ -25,29 +28,60 @@ const Work = () => {
                 Slide Depot
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <h1 className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Tech Stack - Django, ReactJS
+                </h1>
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${Slidedepot})` }}
+            style={{ backgroundImage: `url(${DORSHS})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Slide Depot
+                DORSHS
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <h1 className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Tech Stack - Firebase, ReactJS
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${GENCO})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Genco Pizza
+              </span>
+              <div className="pt-8 text-center">
+                <h1 className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Tech Stack - Firebase, ReactJS
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${CROWDPH})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Crowd PH
+              </span>
+              <div className="pt-8 text-center">
+                <h1 className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Tech Stack - Firebase, ReactJS
+                </h1>
               </div>
             </div>
           </div>
